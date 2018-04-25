cc8f232b2ff18a234358ac8d544704b38cb15cd6
Joe Crawford <joe@artlung.com>
2018-04-25 13:19:25 -0700

    Using namespace instead removing "require_once"

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+
 class HelloWorldTest extends PHPUnit_Framework_TestCase
 {
     /**
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once ('PHPUnit/Framework/TestCase.php');
-
 function loader($class)
 {
     $file = $class . '.php';
